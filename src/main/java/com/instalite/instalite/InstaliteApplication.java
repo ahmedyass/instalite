c8e74a922dc5e1ddd318b101d2fa0263a95668ba
Romain Hebert <27229744+romain-hebert@users.noreply.github.com>
2023-12-31 17:38:55 +0100

    feat(env): wait for database, create admin account

--- src/main/java/com/instalite/instalite/InstaliteApplication.java
+++ src/main/java/com/instalite/instalite/InstaliteApplication.java
@@ -1,7 +1,13 @@
 package com.instalite.instalite;
 
+import com.instalite.instalite.dto.RegisterDto;
+import com.instalite.instalite.model.UserRole;
+import com.instalite.instalite.repository.UserRepository;
+import com.instalite.instalite.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -12,4 +18,35 @@ public class InstaliteApplication {
         SpringApplication.run(InstaliteApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            UserService service,
+            UserRepository repository
+    ) {
+        return args -> {
+            repository.findByUsername("admin").ifPresentOrElse(
+                action -> System.out.println("\"admin\" account already exists"),
+                () -> createAdminAccount(service, repository)
+            );
+            System.out.println("\n\nAdmin password: password\n" +
+                "Unless it has already been changed\n" +
+                "If not, please change it immediately\n\n");
+        };
+    }
+
+    private void createAdminAccount(UserService service, UserRepository repository) {
+        var admin = RegisterDto.builder()
+            .username("admin")
+            .password("password")
+            .email("admin@mail.com")
+            .build();
+
+        service.register(admin);
+
+        repository.findByUsername("admin").ifPresent(user -> {
+            user.setRole(UserRole.ADMINISTRATOR);
+            repository.save(user);
+        });
+        System.out.println("Created \"admin\" account");
+    }
 }
